7e86ac5e7a2e360776f75b6f87d673d7534b54ac
Dane Cronin <danecronin@gmail.com>
2022-11-22 15:33:37 -0700

    added handleSubmit function for submit button logic

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,25 +1,39 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 //Contact Form Function
 
 function ContactForm() {
+    const { name, email, message} = formState;
+    const [formState, setFormState] = useState({name: '', email: '', message: ''});
+
+    //Function to handle keystrokes into NAME, EMAIL and MESSAGE input fields
+    function handleChange(e) {
+        setFormState({...formState, [e.target.name]: e.target.value})
+    }
+
+
+    //function to handle submit button 
+    function handleSubmit(e) {
+        e.preventDefault();
+        console.log(formState);
+    }
 
 
     return (
         <section>
             <h1> Contact Me</h1>
-            <form id="contact-form">
+            <form id="contact-form" onSubmit={handleSubmit}>
                 <div>
                     <label htmlFor="name">Name:</label>
-                    <input type="text" name="name" />
+                    <input type="text" name="name" defaultValue= {name} onChange={handleChange}/>
                 </div>
                 <div>
                     <label htmlFor="email">Email address:</label>
-                    <input type="email" name="email" />
+                    <input type="email" name="email" defaultValue= {email} onChange={handleChange}/>
                 </div>
                 <div>
                     <label htmlFor="message">Message:</label>
-                    <textarea name="message" rows="5" />
+                    <textarea name="message" defaultValue={message} onChange={handleChange} rows="5" />
                 </div>
 
                 <buton type="submit">Submit</buton>
